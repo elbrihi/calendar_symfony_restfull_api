bb0c1e5d2afeac23c6b3147da30ca4e81d74cc90
Elbrihi yasine <elbrihiyasien3@gmail.com>
2020-03-18 15:33:38 +0100

    updated config.yml by adding fost_rest to active listner

--- src/SportRadar/CalendarBundle/Controller/SportController.php
+++ src/SportRadar/CalendarBundle/Controller/SportController.php
@@ -5,7 +5,8 @@ namespace SportRadar\CalendarBundle\Controller;
 use SportRadar\CalendarBundle\Entity\Sport;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Sport controller.
