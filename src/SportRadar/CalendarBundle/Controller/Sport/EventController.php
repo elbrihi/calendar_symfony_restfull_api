881b2ebfa0c5d667ff679b1015173f7f737a48d7
Elbrihi yasine <elbrihiyasien3@gmail.com>
2020-03-22 22:55:56 +0100

    created EventManger service by creating src\SportRadar\CalendarBundle\Service\EventManager.php updating the contains of service.yml and using     one method saveEvent and getAllEvents called from EventManager by postEventtAction and getEventtAction

--- src/SportRadar/CalendarBundle/Controller/Sport/EventController.php
+++ src/SportRadar/CalendarBundle/Controller/Sport/EventController.php
@@ -29,12 +29,8 @@ class EventController extends Controller
      */
     public function getEventsAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $events = $this->get('doctrine.orm.entity_manager')
-                ->getRepository('SportRadarCalendarBundle:Event')
-                ->findAll();
-        return $events;
+        return $this->get('sport_radar_calendar.event_manager')->getAllEvents();
+        
     }
     /**
      * 
@@ -44,64 +40,8 @@ class EventController extends Controller
     
     public function postEventAction(Request $request)
     {
+        return $this->get('sport_radar_calendar.event_manager')->saveEvent($request);
         
-        $sport = $this->get('doctrine.orm.entity_manager')
-        ->getRepository('SportRadarCalendarBundle:Sport')
-        ->find($request->get('id'));
-
-        if (empty($sport)) {
-            return $this->sportNotFound();
-        }
-        $event = new Event();
-        
-        $form = $this->createForm(EventType::class, $event);
-        
-        
-        $form->submit($request->request->all());
-        
-        $event->setSport($sport);
-      
-        $date = $event->getDate();
-
-        $event->setDate($this->get('sport_radar_calendar.date')->dateToString($date));
-
-        if ($form->isValid()) {
-            $event->setSport($sport);
-            
-            $em = $this->get('doctrine.orm.entity_manager');
-            
-            $em->persist($event);
-
-            $em->flush();
-            
-            return  $event;
-        } else {
-            return $form;
-        }
     }
-     /**
-     * 
-     * @Rest\View(serializerGroups={"event"})
-     * @Rest\Get("/sport/{id}/events", name="event")
-     */
-    public function getEventAction(Request $request)
-    {
-        $sport = $this->get('doctrine.orm.entity_manager')
-                ->getRepository('SportRadarCalendarBundle:Sport')
-                ->find($request->get('id')); // L'identifiant en tant que paramétre n'est plus nécessaire
-        /* @var $place Place */
-
-        if (empty($sport)) {
-            return $this->sportNotFound();
-        }
-
-        return $sport;
-    }
-
-    
-    private function sportNotFound()
-    {
-        return \FOS\RestBundle\View\View::create(['message' => 'Sport not found'], Response::HTTP_NOT_FOUND);
-    }
-
+     
 }
---
+++ src/SportRadar/CalendarBundle/Service/EventManager.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SportRadar\CalendarBundle\Service;
+
+use SportRadar\CalendarBundle\Entity\Event;
+use Symfony\Component\Form\FormFactory ;
+use SportRadar\CalendarBundle\Form\Type\EventType;
+use SportRadar\CalendarBundle\Service\Date;
+
+class EventManager
+{
+    private $em;
+
+    private $factoryForm ;
+
+    private $date ; 
+
+    public function __construct(
+        \Doctrine\ORM\EntityManager $entityManager,
+        
+        FormFactory $factoryForm,
+        
+        Date $date
+        )
+    {
+        
+        $this->em = $entityManager;
+        $this->factoryForm = $factoryForm;
+        $this->date = $date;
+    }
+
+    public function saveEvent($request)
+    {
+        $sport = $this->em
+        ->getRepository('SportRadarCalendarBundle:Sport')
+        ->find($request->get('id'));
+       ;
+        $event = new Event();
+        
+        $form = $this->factoryForm->create(EventType::class, $event);
+        
+        
+        $form->submit($request->request->all());
+        
+        $event->setSport($sport);
+      
+        $date = $event->getDate();
+
+        
+        $event->setDate($this->date->dateToString($date));
+        
+        if ($form->isValid()) {
+            $event->setSport($sport);
+            
+            
+            $this->em->persist($event);
+
+            $this->em->flush();
+            
+            return  $event;
+        } else {
+            return $form;
+        }
+       
+    }
+
+    public function getAllEvents()
+    {
+       
+
+        $sports = $this->em
+                ->getRepository('SportRadarCalendarBundle:Sport')
+                ->findAll();
+        return $sports;
+    }
+}
